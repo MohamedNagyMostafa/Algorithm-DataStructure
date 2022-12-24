5cc52c5518a986f9ed2a36b6d0566d16a6dfa439
MohamedNagyMostafa <mohamed.nagy@ku.ac.ae>
2022-12-24 11:42:33 +0400

    find first binary search

--- basic algorithms/binary_search.py
+++ basic algorithms/binary_search.py
@@ -15,8 +15,18 @@ def binary_search_rec(arr, target , start, end):
 def binary_search(array, target):
     return binary_search_rec(array, target, start= 0, end= len(array))
 
+# In case of repeated items
+def find_first(array, target):
+    index = binary_search(array= array, target= target)
+    if index == -1:
+        return -1
+    while array[index - 1] == target:
+        index-=1
 
-target  = 10
-array   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+    return index
+
+target  = 7
+array   =  [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
 
 print(f'Number {target} in array {array} at index({binary_search(array= array, target= target)})')
+print(f'Number {target} in array {array} at first index({find_first(array= array, target= target)})')
